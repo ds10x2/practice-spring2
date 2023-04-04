05efa07c201a80aef0afbf466da16d5d4ed7983c
ds10x2 <padasol@khu.ac.kr>
2023-04-04 19:55:26 +0900

    싱글톤

--- sp5-chap02/src/main/java/org/example/Main.java
+++ sp5-chap02/src/main/java/org/example/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args){
         AnnotationConfigApplicationContext ctx =
                     new AnnotationConfigApplicationContext(AppContext.class);
-        Greeter g = ctx.getBean("greeter", Greeter.class);
+        Greeter g1 = ctx.getBean("greeter", Greeter.class);
         //getBean 메서드는 greeter() 메서드가 생성한 Greeter 객체를 리턴
-
-        String msg = g.greet("spring");
-        System.out.println(msg);
+        Greeter g2 = ctx.getBean("greeter", Greeter.class);
+        System.out.println("(g1 == g2)" + (g1 == g2)); //true를 반환함
+        //별도 설정을 하지 않을 경우 스프링은 한 개의 빈 객체를 생성 -> 싱글톤 범위를 갖는다
         ctx.close();
     }
 }
